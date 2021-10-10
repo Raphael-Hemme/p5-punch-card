9a6f01b139bfa0dea0d81d732c07bdcd2780f964
Raphael Hemme <hemme.raphael@gmail.com>
2021-10-10 19:39:45 +0200

    Change color generation from RGB to HSL and change canvas size

--- sketch.js
+++ sketch.js
@@ -2,13 +2,24 @@ const dotArr = [];
 
 
 function setup() {
-  createCanvas(400, 400);
-  background(220);
+  // createCanvas(400, 400);
+  createCanvas(800, 300);
+  // background(220);
 
-  const randomR = Math.floor(random(0, 256));
+/*   const randomR = Math.floor(random(0, 256));
   const randomG = Math.floor(random(0, 256));
   const randomB = Math.floor(random(0, 256));
-  const randomA = Math.floor(random(50, 256));
+  const randomA = Math.floor(random(50, 256)); */
+
+  const randH = Math.floor(random(0, 361))
+  const sBg = 20;
+  const sDots = 100
+  const randLBg = Math.floor(random(10, 90))
+  const randLDots = randLBg < 50 ? Math.floor(random(50, 80)) : Math.floor(random(20, 50))
+
+  colorMode(HSL)
+
+  background(randH, sBg, randLBg);
 
   for (let i = 20; i < width - 10; i+= 20) {
     for (let j = 20; j < height; j+= 20) {
@@ -22,7 +33,7 @@ function setup() {
       
     }
   }
-  fill(randomR, randomG, randomB, randomA);
+  fill(randH, sDots, randLDots);
   noStroke();
   for(el of dotArr) {
     circle(el.xPos, el.yPos, 10)
