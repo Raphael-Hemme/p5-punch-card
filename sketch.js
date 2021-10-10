c919659211622908cbeddc2d3812ff2371a23e8f
Raphael Hemme <hemme.raphael@gmail.com>
2021-10-10 18:46:51 +0200

    Add random RGB color to use for dots and reduce random variation in xAxis distance between points

--- sketch.js
+++ sketch.js
@@ -1,21 +1,28 @@
 const dotArr = [];
 
+
 function setup() {
   createCanvas(400, 400);
   background(220);
-  for (let i = 5; i < width - 20; i+= 20) {
+
+  const randomR = Math.floor(random(0, 256));
+  const randomG = Math.floor(random(0, 256));
+  const randomB = Math.floor(random(0, 256));
+  const randomA = Math.floor(random(50, 256));
+
+  for (let i = 20; i < width - 10; i+= 20) {
     for (let j = 20; j < height; j+= 20) {
-      const randomXDistRange = Math.floor(random(10, 20))
+      // const randomXDistRange = Math.floor(random(10, 20))
       if(Math.random() > 0.5) {
         dotArr.push({
-          xPos: i + randomXDistRange,
+          xPos: i, // + randomXDistRange,
           yPos: j
         });
       }
       
     }
   }
-  fill(134, 152, 200, 255);
+  fill(randomR, randomG, randomB, randomA);
   noStroke();
   for(el of dotArr) {
     circle(el.xPos, el.yPos, 10)
